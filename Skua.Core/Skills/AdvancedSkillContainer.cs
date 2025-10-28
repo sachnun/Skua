7e4370828d6c559e41445ff7f99ca58801cc5aa8
SharpTheNightmare <koen.t04@gmail.com>
2025-10-28 02:12:25 -0500

    Update AdvancedSkillContainer.cs

--- Skua.Core/Skills/AdvancedSkillContainer.cs
+++ Skua.Core/Skills/AdvancedSkillContainer.cs
@@ -314,9 +314,11 @@ public class AdvancedSkillContainer : ObservableRecipient, IAdvancedSkillContain
                 if (!jsonPath.EndsWith(".json"))
                     jsonPath = Path.ChangeExtension(jsonPath, ".json");
                 SaveToJson(jsonPath);
+                _jsonConfig = null;
             }
             catch
             {
+                _jsonConfig = null;
             }
         }, _saveCts.Token);
     }
